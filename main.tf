107b2274adc0120e0fb9515788d25bfbee749498
studyrarabattu <study@rarabattu.com>
2023-01-03 18:11:21 +0000

    Changed the region and AMI id

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0574da719dca65348"
+  ami           = "ami-0cca134ec43cf708f"
   instance_type = lookup(var.my-var, terraform.workspace)
 
   tags = {
